72d1076c49e5d91d86ddbd73010571e1eb0b9a5b
S-Skaggs <s-skaggs@outlook.com>
2024-04-05 18:12:08 -0400

    FEAT: Completed Assignment 3.2 Unit Testing.

--- week-3/distance-calculator/src/distance-calculator.js
+++ week-3/distance-calculator/src/distance-calculator.js
@@ -1,5 +1,30 @@
 function calculateDistance(planet1, planet2) {
-  // TODO: Implement this function
+  // Declare an associative array to hold planet distances from the sun based on
+  // https://www.jpl.nasa.gov/edu/pdfs/scaless_reference.pdf
+  let distanceFromSun = {
+    "Mercury": 0.39,
+    "Venus": 0.72,
+    "Earth": 1,
+    "Mars": 1.52,
+    "Jupiter": 5.2,
+    "Saturn": 9.54,
+    "Uranus": 19.2,
+    "Neptune": 30.06
+  };
+
+  // Test that both planets are in the array
+  if(planet1 in distanceFromSun === false) {
+    throw new RangeError("Planet1 must be a valid planet name from our solar system.");
+  }
+
+  if(planet2 in distanceFromSun === false) {
+    throw new RangeError("Planet2 must be a valid planet name from our solar system.");
+  }
+
+  // Calculate planet distances by finding the difference of their distances to the sun
+  // Use Math's absolute value function to accommodate any planet order.
+  // Fix the result to two decimal places
+  return Math.abs(distanceFromSun[planet1] - distanceFromSun[planet2]).toFixed(2) + " AU";
 }
 
 module.exports = calculateDistance;
--- week-3/distance-calculator/test/distance-calculator.spec.js
+++ week-3/distance-calculator/test/distance-calculator.spec.js
@@ -1,8 +1,58 @@
 const assert = require('assert');
 const calculateDistance = require('../src/distance-calculator');
 
-function testFunctionDescription() {
-  // TODO: Implement this function
+function testEarthToMars() {
+  try {
+    assert.strictEqual(calculateDistance("Earth", "Mars"), "0.52 AU");
+    console.log("testEarthToMars - PASSED!");
+    return true;
+  } catch(err) {
+    console.error(`Failed testEarthToMars: ${err.message}`);
+    return false;
+  }
+}
+
+function testPlanet1NotAPlanet() {
+  try {
+    assert.strictEqual(calculateDistance("foo", "Earth"), -1);
+    throw new Error("Failed to detect invalid planet name!");
+  } catch(err) {
+    if(err.Name = "RangeError" && err.message === "Planet1 must be a valid planet name from our solar system.") {
+      console.log("testPlanet1NotAPlanet - PASSED!");
+      return true;
+    }
+    console.error(`Failed testPlanet1NotAPlanet: ${err.message}`);
+    return false;
+  }
+}
+
+function testPlanet2NotAPlanet() {
+  try {
+    assert.strictEqual(calculateDistance("Earth", "Foo"), -1);
+    throw new Error("Failed to detect invalid planet name!");
+  } catch(err) {
+    if(err.Name = "RangeError" && err.message === "Planet2 must be a valid planet name from our solar system.") {
+      console.log("testPlanet2NotAPlanet - PASSED!");
+      return true;
+    }
+    console.error(`Failed testPlanet2NotAPlanet: ${err.message}`);
+    return false;
+  }
+}
+
+function testMercuryToNeptune() {
+  try {
+    assert.strictEqual(calculateDistance("Mercury", "Neptune"), "29.67 AU");
+    console.log("testMercuryToNeptune - PASSED!");
+    return true;
+  } catch(err) {
+    console.error(`Failed testMercuryToNeptune: ${err.message}`);
+    return false;
+  }
 }
 
 // Call your test functions here
+testEarthToMars();
+testPlanet1NotAPlanet();
+testPlanet2NotAPlanet();
+testMercuryToNeptune();
