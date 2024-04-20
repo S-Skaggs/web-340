8908e4e98dba3d0a4b73e807092e96e581d04754
S-Skaggs <s-skaggs@outlook.com>
2024-04-20 16:02:05 -0400

    FEAT: Completed superhero-checker example and pie-baker assignment.

--- week-5/pie-baker/src/pie.js
+++ week-5/pie-baker/src/pie.js
@@ -1,13 +1,34 @@
 /**
- * Author:
- * Date:
- * File Name:
- * Description:
+ * Author: Sheldon Skaggs
+ * Date: 4/19/2024
+ * File Name: pie.js
+ * Description: File for the bakPie module
  */
 "use strict";
 
 function bakePie(pieType, ingredients) {
-  // Your code here
+  let hasEssential = true;
+  let essentials = [
+    "flour",
+    "sugar",
+    "butter"
+  ];
+
+  // Convert ingredients input to lowercase
+  let lcIngredients = ingredients.map(i => i.toLowerCase());
+
+  for(let ingredient of essentials) {
+    if(!lcIngredients.includes(ingredient)) {
+      hasEssential = false;
+    }
+  }
+
+  if(hasEssential) {
+    console.log("success");
+  } else {
+    console.log("unsuccessful");
+    process.exit(1);
+  }
 }
 
 module.exports = { bakePie };
--- week-5/pie-baker/test/pie.spec.js
+++ week-5/pie-baker/test/pie.spec.js
@@ -1,12 +1,68 @@
 /**
- * Author:
- * Date:
- * File Name:
- * Description:
+ * Author: Sheldon Skaggs
+ * Date: 4/19/2024
+ * File Name: pie.spec.js
+ * Description: Test suite for the pie-baker
  */
 
 "use strict";
 
-const { bakePie } = require("../src/pie");
+const pieBaker = require("../src/pie");
 
-// Your tests here
+// Test suite for bakePie
+describe("pieBaker", () => {
+  let log;
+  let exit;
+
+  // Runs before each test
+  beforeEach(() => {
+    // Spy on the console's log method
+    log = jest.spyOn(console, "log");
+
+    // Spy on the exit process to prevent the test from exiting.
+    exit = jest.spyOn(process, "exit").mockImplementation((code) => code);
+  });
+
+  // Runs after each test
+  afterEach(() => {
+    // Restore the spies created with spyOn
+    log.mockRestore();
+    exit.mockRestore();
+  })
+
+  test("successfully baked pie", () => {
+    let pieIngredients = ["flour", "sugar", "butter"];
+    let pieName = "myFakePie";
+
+    pieBaker.bakePie(pieName, pieIngredients);
+    expect(log).toHaveBeenCalledWith("success");
+    expect(exit).not.toHaveBeenCalled();
+  });
+
+  test("pie not baked", () => {
+    let pieIngredients = ["flour", "sugar"];
+    let pieName = "myFakePie";
+
+    pieBaker.bakePie(pieName, pieIngredients);
+    expect(log).toHaveBeenCalledWith("unsuccessful");
+    expect(exit).toHaveBeenCalledWith(1);
+  });
+
+  test("successfully baked pie ignoring ingredient case", () => {
+    let pieIngredients = ["Flour", "sUgar", "buTTer"];
+    let pieName = "myFakePie";
+
+    pieBaker.bakePie(pieName, pieIngredients);
+    expect(log).toHaveBeenCalledWith("success");
+    expect(exit).not.toHaveBeenCalled();
+  });
+
+  test("successfully baked pie with extra ingredients", () => {
+    let pieIngredients = ["Flour", "sUgar", "buTTer", "1 can of pumpkin", "1 can, 14oz, sweetened condensed milk", "2 large eggs", "1 tsp ground cinnamon", "1/2 tsp ground ginger", "1/2 tsp ground nutmeg", "1/2 tsp salt"];
+    let pieName = "myFakePie";
+
+    pieBaker.bakePie(pieName, pieIngredients);
+    expect(log).toHaveBeenCalledWith("success");
+    expect(exit).not.toHaveBeenCalled();
+  });
+});
