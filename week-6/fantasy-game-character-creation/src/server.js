3450fe79bbde15b061bf3a6e1d5eca731e14a8da
S-Skaggs <s-skaggs@outlook.com>
2024-04-24 20:38:58 -0400

    FEAT: completed fantasy-game-character-creation exercise.

--- week-6/fantasy-game-character-creation/src/server.js
+++ week-6/fantasy-game-character-creation/src/server.js
@@ -1,10 +1,47 @@
+"use strict";
+
 const http = require('http');
 const url = require('url');
 
-// TODO: Implement your server here
+// Create character object
+let newCharacter = {
+  characterClass: "",
+  gender: "",
+  funFact: ""
+};
 
 const server = http.createServer((req, res) => {
-  // TODO: Implement your routes here
+  // Parse the URL and query parameters
+  const parsedUrl = url.parse(req.url, true);
+  // Get the path
+  const pathname = parsedUrl.pathname;
+  // Get the query parameters as an object
+  const query = parsedUrl.query;
+
+  if (pathname === "/create" && req.method === "POST"){
+    // Populate newCharacter
+    newCharacter.characterClass = query.class;
+    newCharacter.gender = query.gender;
+    newCharacter.funFact = query.funFact;
+
+    res.writeHead(201);
+    res.end(`Created a ${newCharacter.gender} ${newCharacter.characterClass} who ${newCharacter.funFact}.`);
+  } else if (pathname === "/confirm" && req.method === "POST" && newCharacter.characterClass !== ""){
+    // Confirm character creation
+    res.writeHead(200);
+    res.end("Character has been created.");
+  } else if (pathname === "/confirm" && req.method === "POST" && newCharacter.characterClass === ""){
+    // No character created
+    res.writeHead(501);
+    res.end("Character has NOT been created.");
+  } else if(pathname === "/view" && req.method === "GET") {
+    // View character
+    res.writeHead(200);
+    res.end(`Class: ${newCharacter.characterClass} Gender: ${newCharacter.gender} Fun Fact: ${newCharacter.funFact}.`);
+  } else {
+    res.writeHead(404);
+    res.end();
+  }
 });
 
 server.listen(3000, () => {
--- week-6/fantasy-game-character-creation/test/server.spec.js
+++ week-6/fantasy-game-character-creation/test/server.spec.js
@@ -1,4 +1,124 @@
+"use strict";
+
 const http = require('http');
 const server = require('../src/server');
 
-// TODO: Implement your tests here
+// Create test Suite
+describe("server", () => {
+  // After all tests close the server
+  afterAll(() => {
+    server.close();
+  });
+
+  // Using the done argument in a Jest test
+  // Will make Jest wait until the done callback is called before finishing the test.
+  test("responds to /confirm POST request no character", done => {
+    // Create options object
+    const options = {
+      hostname: "localhost",
+      port: 3000,
+      path: "/confirm",
+      method: "POST"
+    };
+
+    // Create the request object
+    const req = http.request(options, res => {
+      // Create variable to hold response data
+      let data = "";
+
+      // Create listener for response data event
+      res.on("data", chunk => {
+        data += chunk;
+      });
+
+      // Create listener for response end event
+      res.on("end", () => {
+        expect(res.statusCode).toBe(501);
+        expect(data).toBe("Character has NOT been created.");
+        done();
+      });
+    });
+    req.end();
+  });
+
+  // Using the done argument in a Jest test
+  // Will make Jest wait until the done callback is called before finishing the test.
+  test("responds to /create POST request with query parameter", done => {
+    // Create options object
+    const options = {
+      hostname: "localhost",
+      port: 3000,
+      path: "/create?class=Mage&gender=Female&funFact=stutters",
+      method: "POST"
+    };
+
+    // Create the request object
+    const req = http.request(options, res => {
+      // Create variable to hold response data
+      let data = "";
+
+      // Create listener for response data event
+      res.on("data", chunk => {
+        data += chunk;
+      });
+
+      // Create listener for response end event
+      res.on("end", () => {
+        expect(res.statusCode).toBe(201);
+        expect(data).toBe("Created a Female Mage who stutters.");
+        done();
+      });
+    });
+    req.end();
+  });
+
+  // Using the done argument in a Jest test
+  // Will make Jest wait until the done callback is called before finishing the test.
+  test("responds to /confirm POST request", done => {
+    // Create options object
+    const options = {
+      hostname: "localhost",
+      port: 3000,
+      path: "/confirm",
+      method: "POST"
+    };
+
+    // Create the request object
+    const req = http.request(options, res => {
+      // Create variable to hold response data
+      let data = "";
+
+      // Create listener for response data event
+      res.on("data", chunk => {
+        data += chunk;
+      });
+
+      // Create listener for response end event
+      res.on("end", () => {
+        expect(res.statusCode).toBe(200);
+        expect(data).toBe("Character has been created.");
+        done();
+      });
+    });
+    req.end();
+  });
+
+  // Using the done argument in a Jest test
+  // Will make Jest wait until the done callback is called before finishing the test.
+  test("responds to /view GET request", done => {
+    http.get("http://localhost:3000/view", res => {
+      let data = "";
+
+      res.on("data", chunk => {
+        data += chunk;
+      });
+
+      res.on("end", () => {
+        expect(res.statusCode).toBe(200);
+        expect(data).toBe("Class: Mage Gender: Female Fun Fact: stutters.");
+        done();
+      });
+    });
+  });
+
+});
