5d45ebb58d1dfd1c07d583778eabf17ad70cfd03
S-Skaggs <s-skaggs@outlook.com>
2024-05-04 14:32:40 -0400

    FEAT: Added chapter examples; jokes-stream, movie-processor. Started Assignment 7.2 - Streams

---
+++ week-7/chapter/jokes-stream/test/jokes-streamer.spec.js
@@ -0,0 +1,26 @@
+"use strict";
+
+const { JokesStream } = require ("../src/jokes-streamer");
+
+describe ("JokesStream", () => {
+  // Output Jokes correctly
+  test("should output jokes correctly", (done) => {
+    const jokes = [
+      "What do you calla fake noodle? An impasta!",
+      "What do you call a bear with no teeth? A gummy bear!",
+      "Why don't scientists trust atoms? Because they make up everything!"
+    ];
+
+    const jokesStream = new JokesStream(jokes.slice().reverse());
+
+    let output = "";
+    jokesStream.on("data", (chunk) => {
+      output += chunk;
+    });
+
+    jokesStream.on("end", () => {
+      expect(output).toBe(jokes.join("\n") + "\n");
+      done();
+    });
+  });
+});
