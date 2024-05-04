626ac4ba404d15ab71393297c59acafc7ca63fd6
S-Skaggs <s-skaggs@outlook.com>
2024-05-04 15:35:11 -0400

    FEAT: Completed Assignment 7.2 - Streams.

--- week-7/fantasy-character-creation-stream/src/character-creator.js
+++ week-7/fantasy-character-creation-stream/src/character-creator.js
@@ -3,15 +3,39 @@ const { Duplex } = require('stream');
 class CharacterCreator extends Duplex {
   constructor(options) {
     super(options);
-    // TODO: Initialize your class here
+    this.queue = [];
+
+    // Object to hold character data
+    this.newCharacter = {
+      class: "",
+      gender: "",
+      fact: ""
+    };
   }
 
   _write(chunk, encoding, callback) {
-    // TODO: Implement your _write method here
+    const sentCharacter = chunk.toString().trim().split(",");
+
+    if(sentCharacter.length != 3) {
+      callback(new Error("Invalid or missing character data"));
+      return;
+    }
+
+    // Convert character from an array of strings to an object
+    this.newCharacter.class = sentCharacter[1].trim();
+    this.newCharacter.gender = sentCharacter[0].trim();
+    this.newCharacter.fact = sentCharacter[2].trim();
+
+    this.queue.push(`Created a ${this.newCharacter.gender} gendered ${this.newCharacter.class} who ${this.newCharacter.fact}.`);
+    callback();
   }
 
   _read(size) {
-    // TODO: Implement your _read method here
+    if(this.queue.length) {
+      this.push(this.queue.shift() + "\n");
+    } else {
+      this.push(null);
+    }
   }
 }
 
--- week-7/fantasy-character-creation-stream/test/character-creator.spec.js
+++ week-7/fantasy-character-creation-stream/test/character-creator.spec.js
@@ -8,14 +8,45 @@ describe('CharacterCreator', () => {
   });
 
   test("should process data correctly when written to", (done) => {
-    // TODO: Write your test here
+    // Create a character object
+    const testCharacter = {
+      class: "Warrior",
+      gender: "Male",
+      fact: "Is afraid of spiders"
+    };
+
+    characterCreator.write(`${testCharacter.gender}, ${testCharacter.class}, ${testCharacter.fact}`);
+
+    characterCreator.on("data", (data) => {
+      expect(data.toString().trim()).toBe(`Created a ${testCharacter.gender} gendered ${testCharacter.class} who ${testCharacter.fact}.`);
+      done();
+    });
   });
 
   test("should emit 'error' when invalid data is written", (done) => {
-    // TODO: Write your test here
+    characterCreator.on("error", (err) => {
+      expect(err.message).toBe("Invalid or missing character data");
+      done();
+    });
+
+    characterCreator.write("");
   });
 
   test("should transform data correctly when written to", (done) => {
-    // TODO: Write your test here
+    // Create a character object
+    const testCharacter = {
+      class: "Mage",
+      gender: "Female",
+      fact: "Collects feathers."
+    };
+    const dataToSend = `${testCharacter.gender}, ${testCharacter.class}, ${testCharacter.fact}`;
+    const expectedOutput = `Created a ${testCharacter.gender} gendered ${testCharacter.class} who ${testCharacter.fact}.`;
+
+    characterCreator.write(dataToSend, "utf8", () => {
+      characterCreator.on("data", (data) => {
+        expect(data.toString().trim()).toBe(expectedOutput);
+        done();
+      });
+    });
   });
 });
